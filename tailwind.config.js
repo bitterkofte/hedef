479f6803d961535ad32afd734d09cb64189daa2c
Hasan Talha Çelik <35665454+bitterkofte@users.noreply.github.com>
2024-03-14 02:00:01 +0300

    styling updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        'u-2xl': {'min': '1536px'},
+        'u-xl': {'min': '1280px'},
+        'u-lg': {'min': '1024px'},
+        'u-md': {'min': '768px'},
+        'u-sm': {'min': '501px'},
+
+        'd-2xl': {'max': '1535px'},
+        'd-xl': {'max': '1279px'},
+        'd-lg': {'max': '1023px'},
+        'd-md': {'max': '767px'},
+        'd-sm': {'max': '500px'},
+      },
       colors: {
         neutral: {
           750: "#303030"
